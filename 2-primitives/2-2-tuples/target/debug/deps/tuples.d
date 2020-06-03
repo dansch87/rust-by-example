b5419df653e74944edc9ec66c8ef03ba37b56a39
Daniel Schulz <daniel.schulz.lg@gmail.com>
2020-06-03 20:01:30 +0200

    Add 2-2 exerc

---
+++ 2-primitives/2-2-tuples/target/debug/deps/tuples.d
@@ -0,0 +1,5 @@
+C:\Users\dansch\Desktop\rust-by-example\2-primitives\2-2-tuples\target\debug\deps\tuples.exe: src\main.rs
+
+C:\Users\dansch\Desktop\rust-by-example\2-primitives\2-2-tuples\target\debug\deps\tuples.d: src\main.rs
+
+src\main.rs:
---
+++ 2-primitives/2-2-tuples/target/debug/tuples.d
@@ -0,0 +1,1 @@
+C:\Users\dansch\Desktop\rust-by-example\2-primitives\2-2-tuples\target\debug\tuples.exe: C:\Users\dansch\Desktop\rust-by-example\2-primitives\2-2-tuples\src\main.rs

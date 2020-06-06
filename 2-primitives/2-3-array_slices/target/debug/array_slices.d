eaa35d6434ceb4786001b5b3734e9e99e814283c
Daniel Schulz <daniel.schulz.lg@gmail.com>
2020-06-06 11:01:36 +0200

    Add exerc 2-3

---
+++ 2-primitives/2-3-array_slices/target/debug/array_slices.d
@@ -0,0 +1,1 @@
+C:\Users\dansch\Desktop\rust-by-example\2-primitives\2-3-array_slices\target\debug\array_slices.exe: C:\Users\dansch\Desktop\rust-by-example\2-primitives\2-3-array_slices\src\main.rs
---
+++ 2-primitives/2-3-array_slices/target/debug/deps/array_slices.d
@@ -0,0 +1,5 @@
+C:\Users\dansch\Desktop\rust-by-example\2-primitives\2-3-array_slices\target\debug\deps\array_slices.exe: src\main.rs
+
+C:\Users\dansch\Desktop\rust-by-example\2-primitives\2-3-array_slices\target\debug\deps\array_slices.d: src\main.rs
+
+src\main.rs:
